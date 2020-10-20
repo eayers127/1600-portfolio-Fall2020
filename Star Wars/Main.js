7a030acdd8bac54650bd7c5d5841768afe76f60a
Ethan Ayers <ethan.ayers88@gmail.com>
2020-10-20 11:15:58 -0600

    data import

--- Star Wars/Main.js
+++ Star Wars/Main.js
@@ -1,5 +1,17 @@
 import { films } from './films.js'
+import { people } from './people.js'
 
-console.log(films[0])
 
-const MyDocument = document;
+
+//console.log(films[0])
+
+//const filmList = document.querySelector('.filmList')
+
+
+
+films.forEach(film => {
+    let newParagraph = document.body.appendChild(document.createElement('p'))
+    newParagraph.textContent = film.title
+}) 
+
+
