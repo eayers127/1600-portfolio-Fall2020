507f589790cd2eb99617bade65834b17a56ac8a2
Ethan Ayers <ethanayers88@gmail.com>
2020-11-12 10:14:32 -0700

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,46 @@
+import { films } from '../StarWars/films.js'
+import { getLastNumber } from '..utils/index.js'
+
+
+
+//console.log(films[0])
+
+//const filmList = document.querySelector('.filmList')
+
+
+
+/*films.forEach(film => {
+    let newParagraph = document.body.appendChild(document.createElement('p'))
+    newParagraph.textContent = film.title
+}) */
+
+const main = document.querySelector('main')
+
+for (let step=0; step < 7; step++) {
+    let figure = document.createElement('figure')
+    let figImg = document.createElement('img')
+    figImg.src = `https://starwars-visualguide.com/assets/img/films/${step + 1}.jpg`
+    let figCaption = document.createElement('figCaption')
+    
+    const foundFilm = films.find(film => getLastNumber(film.url) == (step + 1))
+    figCaption.textContent = foundFilm.title
+    
+
+    figure.appendChild(figImg)
+    figure.appendChild(figCaption)
+    main.appendChild(figure)
+    
+
+}
+
+/*for (const film of films) {
+    lew newImg = document.creatElement('img') //new image instance
+    newImg.src = 'https://starwars-visualguide.com/assets/img/films/1.jpg' // set the source or will not show
+    //now add the img to the DOM
+    main.appendChild(newImg)
+    console.log(film.title);
+}*/
+
+//https://starwars-visualguide.com/assets/img/characters/1.jpg
+
+
