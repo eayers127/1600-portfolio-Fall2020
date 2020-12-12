d2e8d8cbac3276ae9b00e28fdcab42dfe15f37af
Ethan Ayers <ethan.ayers88@gmail.com>
2020-12-11 22:13:38 -0700

    final pokemon touches

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -61,6 +61,9 @@ function getImageFileName(pokemon){
 
 loadPage()*/
 
+
+
+
 async function getAPIData(url) {
     try {
         const response = await fetch(url)
@@ -72,7 +75,7 @@ async function getAPIData(url) {
 }
 
 function loadPage() {
-    getAPIData(`https://pokeapi.co/api/v2/pokemon`).then
+    getAPIData(`https://pokeapi.co/api/v2/pokemon?&limit=25`).then
         (async (data) => {
             for (const pokemon of data.results) {
                 await getAPIData(pokemon.url).then((pokeData) => {
@@ -119,14 +122,55 @@ function populateCardFront(pokemon) {
     return cardFront
 }
 
-function populateCardBack(pokemon) {
+/*function populateCardBack(pokemon) {
     let cardBack = document.createElement('div')
     cardBack.className = 'card__face card__face--back'
     let backLabel = document.createElement('p')
     backLabel.textContent = `I'm the back of the card`
     cardBack.appendChild(backLabel)
     return cardBack
+}*/
+
+function populateCardBack(pokemon) {
+    let cardBack = document.createElement('div')
+    cardBack.className = 'card__face card__face--back'
+    let backLabel = document.createElement('h3')
+    backLabel.textContent = `Abilities:`
+    let abilityList = document.createElement('ul')
+    pokemon.abilities.forEach(ability => {
+        let abilityName = document.createElement('li')
+        abilityName.textContent = ability.ability.name
+        abilityList.appendChild(abilityName)
+    })
+    let movesLabel = document.createElement('h3')
+    movesLabel.textContent = 'Stats:'
+    let pokeWeight = document.createElement('h5')
+    pokeWeight.textContent = `Weight: ${pokemon.weight} lbs.`
+    let typeLabel = document.createElement('h3')
+    typeLabel.textContent = `Types:`
+    let typeList = document.createElement('ul')
+    pokemon.types.forEach(type => {
+        let typeName = document.createElement('li')
+        typeName.textContent = type.type.name
+        typeList.appendChild(typeName)
+    })
+    
+
+
+    cardBack.appendChild(backLabel)
+    cardBack.appendChild(abilityList)
+    cardBack.appendChild(movesLabel)
+    cardBack.appendChild(pokeWeight)
+    cardBack.appendChild(typeLabel)
+    cardBack.appendChild(typeList)
+    return cardBack
 }
+
+
+
+
+
+
 
 function getImageFileName(pokemon) {
     if (pokemon.id < 10) {
@@ -136,13 +180,52 @@ function getImageFileName(pokemon) {
     }
 }
 
-function Pokemon(name, height, weight, abilities) {
+
+
+/*let newButton = document.querySelector('#newButton')
+newButton.addEventListener('click', () => {
+  addPokemon()
+})*/
+
+
+/*
+function addPokemon() {
+    let newPokemon = new Pokemon(50, 25, 'Ethan', [
+      {
+        ability:
+          { name: 'Thunder Belly' }
+      },
+      {  ability:
+          { name: 'Lightining Fingers' }
+      },
+    ]
+    [ 
+    {
+        type:
+          {name: 'Fire'}
+      }
+    ] 
+      )
+    populatePokeCard(newPokemon)
+  }
+
+  let newButton = document.querySelector('#newPokemon')
+  newButton.addEventListener('click', () => {
+    addPokemon()
+  })
+
+  function Pokemon(name, height, weight, abilities, moves) {
     this.name = name
     this.height = height
     this.weight = weight
     this.abilities = abilities
     this.id = 900
+    this.moves = moves
 }
 
-let thoremon = new Pokemon('Thoremon', 450, 200, ['gorge', 'sleep'])
-console.log(thoremon)
+*/
+
+
+  
+
+
