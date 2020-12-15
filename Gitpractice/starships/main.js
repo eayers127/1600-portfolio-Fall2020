b11a2ac2e41a65813138785947e89ca8cafae45c
Ethan Ayers <ethanayers88@gmail.com>
2020-12-14 19:59:01 -0700

    Add files via upload

---
+++ Gitpractice/starships/main.js
@@ -0,0 +1,82 @@
+import { starships } from '../StarWars/starships.js'
+import { removeChildren, getLastNumber} from '../utils/index.js'
+
+
+const nav = document.querySelector('.nav')
+const navList = document.querySelector('.navList')
+const shipView = document.querySelector('.main')
+
+const dialog = document.querySelector('.modal')
+const closeButton = document.querySelector('.modal-close')
+const modalBackground = document.querySelector('.modal-background')
+
+closeButton.addEventListener('click', () => {
+    dialog.classList.toggle("is-active")
+})
+
+
+modalBackground.addEventListener('click', () => {
+    dialog.classList.toggle("is-active")
+})
+
+function populateNav(starships) {
+    starships.forEach(starship => {
+        let anchorWrap = document.createElement('a')
+        anchorWrap.href = '#'
+        anchorWrap.addEventListener('click', event => {
+            let shipName = event.target.textContent
+            const foundShip = starships.find(ship => ship.name === shipName)
+            populateShipView(foundShip)
+
+        })
+
+
+        let listItem = document.createElement('li')
+        listItem.textContent = starship.name 
+
+        anchorWrap.appendChild(listItem)
+        navList.appendChild(anchorWrap)
+        nav.appendChild(navList)
+    })
+}
+
+function populateShipView(shipData) {
+    removeChildren(shipView)
+    let shipImage = document.createElement('img')
+    let shipNum = getLastNumber(shipData.url)
+    shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
+    shipImage.addEventListener('error', () => { 
+    shipImage.hidden = true
+    dialog.classList.toggle("is-active")
+    })
+
+    shipView.appendChild(shipImage)
+
+}
+
+
+
+populateNav(starships)
+
+function addStarField(element, numStars) {
+    element.style.setProperty('background-color', 'black')
+    for (let i = 0; i < numStars; i++){
+        let star = document.createElement('div')
+        star.style.setProperty('position', 'absolute')
+        star.style.setProperty('width', '2px')
+        star.style.setProperty('height', '2px')
+        star.style.setProperty('background-color', 'white')
+        let xy = getRandomPosition()
+    }
+}
+
+function getRandomPosition(){
+    let y = document.body.scrollHeight
+    let x = document.body.scrollWidth
+
+    
+    return [randomX, randomY]
+
+}
+
+addStarField(document.querySelector('body'), 100)
